e258683c6fdcf28d2dfc113b852674ad1995e82c
Shichao <92007182+Shigakki@users.noreply.github.com>
2022-05-05 15:02:55 +0800

    修护interpreter.h
    修护

--- miniSQL/src/Interpreter.h
+++ miniSQL/src/Interpreter.h
@@ -12,22 +12,16 @@ class Interpreter{
 public:
 	Interpreter();
 	~Interpreter();
-<<<<<<< HEAD
+
 	int interpret(string s); //将命令解释给API
-=======
-	int interpret(string s); //设置API
->>>>>>> 35cd068e5018e4c6a538ed8168b735d28635db80
 	string getFilename();
 	void setAPI(API* apiInput);
 
 private:
     API* api;
 	string fileName;
-<<<<<<< HEAD
+
 	string getCommand(string s, int &st); //得到命令中的每个单词
-=======
-	string getCommand(string s, int &st); //获取指令
->>>>>>> 35cd068e5018e4c6a538ed8168b735d28635db80
 };
 
 #endif
